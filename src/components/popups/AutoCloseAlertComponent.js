69d3c87665f0b73c811dfb1830b7e4319dc9a4b9
Margarita Krutikova <margaretkru@gmail.com>
2017-09-23 16:06:32 +0200

    Added cleanup on unmount in the alert component.

--- src/components/popups/AutoCloseAlertComponent.js
+++ src/components/popups/AutoCloseAlertComponent.js
@@ -1,24 +1,39 @@
 import React from 'react';
 
 class AutoCloseAlertComponent extends React.Component {
+    fadeTimeouts = [];
     constructor(props){
         super(props);
 
         this.state = { isVisible: false, fadeClass: "out"};
     }
+    _clearTimeouts = () => {
+        this.fadeTimeouts.forEach(function(timeout) {
+            clearTimeout(timeout);
+        }, this);
+        this.fadeTimeouts.splice(0, this.fadeTimeouts.length);
+    }
     _resetState = () => {
         this.setState({ isVisible: false });
+        this._clearTimeouts();
     }
     componentWillReceiveProps(props) {
         if (props.message) {
             this.setState({ isVisible: true, fadeClass: "in" });
 
-            setTimeout((function() {
+            let fadeStartTimeout = setTimeout((function() {
                 this.setState({ fadeClass: "out" });
-                setTimeout(this._resetState, 500);
+                
+                let fadeEndTimeout = setTimeout(this._resetState, 500);
+                this.fadeTimeouts.push(fadeEndTimeout);
 
             }).bind(this), 3000);
+
+            this.fadeTimeouts.push(fadeStartTimeout);
         }
+    }
+    componentWillUnmount() {
+        this._clearTimeouts();
     }
     render() {
         let alert = <div></div>;
